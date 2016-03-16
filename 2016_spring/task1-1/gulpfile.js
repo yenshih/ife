bf2b2e692e03865ee3410de993a819b5229c5bcc
yenshih <yenshih95@gmail.com>
2016-03-16 20:17:56 +0800

    add some files

--- 2016_spring/task1-1/gulpfile.js
+++ 2016_spring/task1-1/gulpfile.js
@@ -1,30 +1,16 @@
 var gulp = require("gulp"), 
-    sass = require("gulp-ruby-sass"),
-    autoprefixer = require("gulp-autoprefixer"),
-    concat = require("gulp-concat"),
     htmlmin = require("gulp-htmlmin"),
-    minifycss = require("gulp-minify-css"),
     imagemin = require("gulp-imagemin"),
     pngquant = require('imagemin-pngquant'),
     cache = require("gulp-cache"),
     clean = require("gulp-clean"),
-    livereload = require("gulp-livereload"),
-    rename = require("gulp-rename");
+    livereload = require("gulp-livereload");
 
 gulp.task("html", function() {
     return gulp.src("src/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest("dist/"))
 })
-
-gulp.task("styles", function() { 
-    return sass("src/styles/*.scss", {style: "compressed"})
-        .pipe(autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
-        .pipe(concat("main.css"))
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(minifycss())
-        .pipe(gulp.dest("dist/styles"))
-});
 
 gulp.task("images", function() { 
     return gulp.src("src/images/*.{png,jpg,gif,ico}")
@@ -37,17 +23,16 @@ gulp.task("images", function() {
 });
 
 gulp.task("clean", function() { 
-    return gulp.src(["dist/", "dist/styles", "dist/images"], { read: false })
+    return gulp.src(["dist/", "dist/images"], { read: false })
         .pipe(clean());
 });
 
 gulp.task("default", ["clean"], function() { 
-    gulp.start("html", "styles", "images");
+    gulp.start("html", "images");
 });
 
 gulp.task("watch", function() {
     gulp.watch("src/*.html", ["html"]);
-    gulp.watch("src/styles/*.scss", ["styles"]);
     gulp.watch("src/images/*.{png,jpg,gif,ico}", ["images"]);
     livereload.listen();
     gulp.watch(["dist/*"]).on("change", livereload.changed);
