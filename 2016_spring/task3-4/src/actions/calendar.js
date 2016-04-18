0f1d082539f491dc4575d914da7d925424268e81
yenshih <yenshih95@gmail.com>
2016-04-18 23:07:03 +0800

    optimize animation

--- 2016_spring/task3-4/src/actions/calendar.js
+++
@@ -1,9 +0,0 @@
-import * as CalendarActionTypes from "../constants/CalendarActionTypes";
-
-const CalendarActions = {
-	selectDate(year, month, date) {
-        return { type: CalendarActionTypes.SELECT_DATE, year, month, date }
-	}
-};
-
-export default CalendarActions;
--- 2016_spring/task3-4/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import calendar from "./calendar";
-
-const rootReducer = combineReducers({ calendar });
-
-export default rootReducer;
